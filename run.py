5c250250549ac89333fb05ac5543b8b1467debc6
Gumerwest <tomasazcarate@gmail.com>
2025-05-22 23:34:07 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from app import create_app, db
+from flask_migrate import init, migrate, upgrade
+import os
+
+app = create_app()
+
+@app.cli.command()
+def init_db():
+    """Initialize the database."""
+    init()
+    migrate(message='Initial migration')
+    upgrade()
+    print('Initialized the database.')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
