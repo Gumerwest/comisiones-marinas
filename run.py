cb1374672abc3b35b122f6d66ef4d7a7a9c7f500
Gumerwest <tomasazcarate@gmail.com>
2025-05-23 16:59:12 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
-from app import create_app, db
+from app import create_app
 import os
 
 app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    # En entorno de producción, Gunicorn maneja el servidor
-    if os.environ.get('RENDER') is None:
-        app.run(host='0.0.0.0', port=port, debug=True)
-    else:
-        # En Render, Gunicorn se encarga de ejecutar la app
-        pass
+    app.run(host='0.0.0.0', port=port, debug=False)
